217fe3a7eed407057089a59423eb768b984001c7
kristinelm <kristinelm@hotmail.com>
2015-11-01 17:30:13 +0100

    mer generell

--- newtoncotes.m
+++ newtoncotes.m
@@ -1,10 +1,9 @@
 function Q = newtoncotes(f, n, a, b)
     w = dlmread('vekt.txt');
     sum = 0;
-    x = [1, 2, 3, 4, 5];
-    w = ones(5);
-    for i = 1:n
-        sum = sum + (b-a)*w(i)*f(x(i));
+    
+    for i = a:b
+        sum = sum + (b-a)*w(i)*f(i);
     end
     
     Q = sum;
